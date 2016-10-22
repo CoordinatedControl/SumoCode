4cacf88b8a2c1ed1032a17dbedc375d4f1ab2e8b
chaitjo <ckjoshi9@gmail.com>
2016-10-22 23:33:17 +0700

    Testing right+left

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var sumo = require('node-sumo');
 
-var drone = sumo.createClient();
+var drone = sumo.createClient({ip: '192.168.1.1'});
+
+var step=0;
 
 drone.connect(function() {
     console.log('Connected');
     drone.postureJumper();
 
     var interval = setInterval(function() {
-        drone.right(50);
+        step++;
+        if(step%2==0) drone.right(50);
+        else drone.left(50);
         setTimeout(function() {
             drone.forward(10);
         }, 315);
